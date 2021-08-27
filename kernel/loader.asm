a78226f35d1053f55c8b8dcba48c074342bef130
Killaship <69988679+Killaship@users.noreply.github.com>
2021-08-27 12:05:13 -0400

    Update loader.asm

--- kernel/loader.asm
+++ kernel/loader.asm
@@ -1,26 +1,32 @@
-%define drive 0x00
-%define kernel 0x1000
-%define ksect 3
-org 0x7C00   ; add 0x7C00 to label addresses
- bits 16      ; tell the assembler we want 16 bit code
- 
-   mov ax, 0  ; set up segments
-   mov ds, ax
-   mov es, ax
-   mov ss, ax     ; setup stack
-   mov sp, 0x7C00 ; stack grows downwards from 0x7C00
-   
-   
-mov ax,kernel
-mov es,ax
-mov cl,ksect ; sector
-mov al,4 ; number of sectors
-
-call loadsector
-
-jmp kernel:0000
-
   
+%define loc 0x1000
+%define drive 0x80
+%define os_sect 3
+
+[bits 16]
+[org 0]
+
+jmp 0x7c0:start
+
+start:
+
+
+
+
+	mov ax,loc
+	mov es,ax
+	mov cl,os_sect ; sector
+	mov al,4 ; number of sectors
+
+	call loadsector
+
+
+	jmp loc:0000 ; jump to our os
+
+
+done:
+	jmp $
+
 loadsector:
 	mov bx,0
 	mov dl,drive ; drive
@@ -30,17 +36,5 @@ loadsector:
 	int 0x13
 	jc err
 	ret
- 
- err:
-  jmp $
-
- .temp db 0
- 
-   times 510-($-$$) db 0
-   dw 0AA55h ; some BIOSes require this signature
-
-
-
-
-
-
+err:
+	jmp $
