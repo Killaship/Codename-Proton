5122ca5e72b3d995c8b59e200419d7155e0dda38
Killaship <69988679+Killaship@users.noreply.github.com>
2021-08-27 12:21:20 -0400

    Update loader.asm

--- kernel/loader.asm
+++ kernel/loader.asm
@@ -1,5 +1,5 @@
-  
 %define loc 0x1000
+
 %define drive 0x00
 %define os_sect 3
 
@@ -10,16 +10,30 @@ jmp 0x7c0:start
 
 start:
 
+	mov ax,cs
+	mov ds,ax
+	mov es,ax
+
+	mov al,03h
+	mov ah,0
+	int 10h	
 
 
+	mov si,msg
+	call print
+
+	mov ah,0
+	int 16h
 
 	mov ax,loc
 	mov es,ax
 	mov cl,os_sect ; sector
-	mov al,4 ; number of sectors
+	mov al,2 ; number of sectors
 
 	call loadsector
 
+
+	
 
 	jmp loc:0000 ; jump to our os
 
@@ -37,8 +51,24 @@ loadsector:
 	jc err
 	ret
 err:
-	jmp $
+	mov si,erro
+	call print
+	ret
+print:
+	mov bp,sp
+	cont:
+	lodsb
+	or al,al
+	jz dne
+	mov ah,0x0e
+	mov bx,0
+	int 10h
+	jmp cont
+dne:
+	mov sp,bp
+	ret
 
-
+msg db "Booting Successful.,10,13,"Press any key to continue!",10,13,10,13,0
+erro db "Error loading sector.10,13,0
 times 510 - ($-$$) db 0
 dw 0xaa55
