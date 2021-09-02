7ab68d149503788cdd8359b61575a58342aaf5d2
Killaship <69988679+Killaship@users.noreply.github.com>
2021-09-02 09:53:12 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
+rm *.bin
 git pull
-nasm kernel/loader.asm -f bin -o loader.bin
 nasm kernel/kernel.asm -f bin -o kernel.bin
-cat loader.bin kernel.bin > image.bin
 export DISPLAY=:0.0
-qemu-system-i386 -fda image.bin
+qemu-system-i386 -fda kernel.bin
