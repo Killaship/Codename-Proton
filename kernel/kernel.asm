1ff8a8249f229bb00c4b6defe355dfe88fc4614f
Killaship <69988679+Killaship@users.noreply.github.com>
2021-09-02 12:41:14 -0400

    Update kernel.asm

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -110,7 +110,7 @@ stage2:
  
 
  
- welcome db 'Welcome to Codename Proton 0.0.1.', 0x0D, 0x0A, 'Enter "help" for a list of commands.', 0x0D, 0x0A, 0
+ welcome db 'Welcome to Codename Proton 0.0.1.', 0x0D, 0x0A, 'Enter "help" for a list of commands.', 0x0D, 0x0A, 0x0D, 0x0A, 0
  msg_helloworld db 'Hello, World!', 0x0D, 0x0A, 0
  badcommand db 'Bad command entered.', 0x0D, 0x0A, 0
  prompt db 'kernel@Proton: ', 0
