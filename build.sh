3171275639839520ec5fafc84b54dde97ca3eb22
Killaship <69988679+Killaship@users.noreply.github.com>
2022-01-03 16:35:09 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 rm kernel.bin
 git pull
-nasm kernel/boot.asm -f bin -o kernel.bin
+nasm kernel/boot.asm -f bin -o boot.bin
+nasm kernel/kernel.asm -f bin -o kernel.bin
 export DISPLAY=:0.0
-qemu-system-i386 -drive format=raw,file=kernel.bin,index=0,if=floppy
+qemu-system-i386 -drive format=raw,file=boot.bin,index=0,if=floppy -drive format=raw,file=kernel.bin,index=1,if=floppy
+
