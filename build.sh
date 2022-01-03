1cf829a57341ec653e34a394cef5b5f93355b59a
Killaship <69988679+Killaship@users.noreply.github.com>
2022-01-03 15:18:26 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 rm kernel.bin
 git pull
 nasm kernel/boot.asm -f bin -o kernel.bin
-#export DISPLAY=:0.0
-qemu-system-i386 -fda kernel.bin
+export DISPLAY=:0.0
+qemu-system-i386 -drive format=raw,file=helloos.img,index=0,if=floppy
