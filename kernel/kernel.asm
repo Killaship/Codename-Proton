9add7721986f2fce183c6ded83581f83f74104c0
Killaship <69988679+Killaship@users.noreply.github.com>
2022-01-03 16:37:34 -0500

    Update kernel.asm

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -1,40 +1,10 @@
 bits 16
-org 0x7C00
+org 0x1000
 
 
- 	mov ax,cs
-	mov ds,ax
-	mov es,ax
-	mov ax,0x7000	
-	mov ss,ax	; stack segment initialisation
-	mov sp,ss
 
 
-push ax
-mov ax,cs
-mov ds,ax
-mov es,ax
-pop ax
-
-    
-    ; Load stage 2 to memory.
-    mov ah, 0x02
-    mov al, 0x06 ; amount of sectors to load
-    ; This may not be necessary as many BIOS setup is as an initial state.
-    mov dl, 0x00
-    mov ch, 0
-    mov dh, 0
-    mov cl, 2
-    mov bx, stage2
-    int 0x13
-
-    jmp stage2
-
-    ; Magic bytes.    
-    times ((0x200 - 2) - ($ - $$)) db 0x00
-    dw 0xAA55
-
-stage2:
+start:
 
    call cls
    mov si, welcome
