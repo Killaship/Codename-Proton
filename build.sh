5cd95a60c07dab74e0f02bd05a4e6417fc7e3429
Killaship <69988679+Killaship@users.noreply.github.com>
2022-01-11 21:53:02 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,3 @@ nasm kernel/boot.asm -f bin -o boot.bin
 nasm kernel/kernel.asm -f bin -o kernel.bin
 cat boot.bin kernel.bin > os.bin
 qemu-system-i386 -drive format=raw,file=os.bin,index=0,if=floppy -curses
-
