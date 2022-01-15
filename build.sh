ca989770cc8764363a1cf3c5ba93d160b73d6c15
Killaship <69988679+Killaship@users.noreply.github.com>
2022-01-15 08:48:43 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 git pull
-rm boot
-rm os.flp
+rm mikeos.flp
 #nasm kernel/boot.asm -f bin -o boot.bin
 cd src
-nasm src/kernel.asm -f bin -o kernel.bin
+nasm kernel.asm -f bin -o kernel.bin
 cd ..
 #cat boot.bin kernel.bin > os.bin
 bash mOSbuild.sh
