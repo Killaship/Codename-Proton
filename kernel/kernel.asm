8d3a98f1a1365b2d303330fe6bfe433e22e07053
Killaship <69988679+Killaship@users.noreply.github.com>
2022-01-14 21:30:26 -0500

    Update kernel.asm

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -249,5 +249,106 @@ install:
    mov si, uhoh
    call print_string
    ret
+ ; ------------------------------------------------------------------
+; os_string_parse -- Take string (eg "run foo bar baz") and return
+; pointers to zero-terminated strings (eg AX = "run", BX = "foo" etc.)
+; IN: SI = string; OUT: AX, BX, CX, DX = individual strings
+
+os_string_parse:
+	push si
+
+	mov ax, si			; AX = start of first string
+
+	mov bx, 0			; By default, other strings start empty
+	mov cx, 0
+	mov dx, 0
+
+	push ax				; Save to retrieve at end
+
+.loop1:
+	lodsb				; Get a byte
+	cmp al, 0			; End of string?
+	je .finish
+	cmp al, ' '			; A space?
+	jne .loop1
+	dec si
+	mov byte [si], 0		; If so, zero-terminate this bit of the string
+
+	inc si				; Store start of next string in BX
+	mov bx, si
+
+.loop2:					; Repeat the above for CX and DX...
+	lodsb
+	cmp al, 0
+	je .finish
+	cmp al, ' '
+	jne .loop2
+	dec si
+	mov byte [si], 0
+
+	inc si
+	mov cx, si
+
+.loop3:
+	lodsb
+	cmp al, 0
+	je .finish
+	cmp al, ' '
+	jne .loop3
+	dec si
+	mov byte [si], 0
+
+	inc si
+	mov dx, si
+
+.finish:
+	pop ax
+
+	pop si
+	ret
+
+
+
+
+  cls:
+    mov ah, 0x00
+    mov al, 0x03
+    int 0x10
+    ret
+
+  print_hex_byte: 
+   mov [.temp],al
+   shr al,4
+   cmp al,10
+   sbb al,69h
+   das
  
-%include "otherfunctions.asm"
+   mov ah,0Eh
+   int 10h
+ 
+   mov al,[.temp]
+   ror al,4
+   shr al,4
+   cmp al,10
+   sbb al,69h
+   das
+ 
+   mov ah,0Eh
+   int 10h
+ 
+   mov ah, 0x0E
+   mov al, 0x0D
+   int 0x10
+   mov al, 0x0A
+   int 0x10		; newline
+   
+   ret
+
+  .temp db 0
+
+
+
+;getcpu:
+;	xor eax, eax
+;	cpuid
+	
