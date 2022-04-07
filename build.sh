b17969c5461c3cfdc6182c2c3b36d1f73854f8e1
Killaship <69988679+Killaship@users.noreply.github.com>
2022-04-07 12:16:08 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 git pull
-rm os.flp
+
 nasm kernel/boot.asm -f bin -o boot.bin
 cd src
 nasm kernel.asm -f bin -o kernel.bin
 cd ..
 cat boot.bin kernel.bin > os.bin
 #bash mOSbuild.sh
-qemu-system-i386 -drive format=raw,file=os.flp,index=0,if=floppy -curses
+qemu-system-i386 -drive format=raw,file=os.bin,index=0,if=floppy -curses
