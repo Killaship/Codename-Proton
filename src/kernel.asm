1d09f1529685469a66e6ca56be3afafe95f63fe9
Killaship <69988679+Killaship@users.noreply.github.com>
2022-04-07 12:38:07 -0400

    Update kernel.asm

--- src/kernel.asm
+++ src/kernel.asm
@@ -1,11 +1,7 @@
 bits 16
 
 
-;	cli				; Clear interrupts
-;	mov ax, 0
-;	mov ss, ax			; Set stack segment and pointer
-;	mov sp, 0FFFFh
-;	sti				; Restore interrupts
+
 ;	mov ax, 1000h			; Set all segments to match where kernel is loaded
 ;	mov ds, ax			; After this, we don't need to bother with
 ;	mov es, ax			; segments ever again, as the OS and its programs
